2b3c395450de9597aaeb7a802ef8b356002b9b48
pallavag <agpallav@gmail.com>
2025-06-09 19:17:50 -0400

    Initial Commit

---
+++ Apple Intelligence Chat/Apple_Intelligence_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Intelligence_ChatApp.swift
+//  Apple Intelligence Chat
+//
+//  Created by Pallav Agarwal on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Intelligence_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Intelligence Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple Intelligence Chat
+//
+//  Created by Pallav Agarwal on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
